145930eae5c190c6666a3f3d9fc8f278269fb4c8
mohit nandan <mohitnandan81825@gmail.com>
2025-05-25 01:05:38 +0530

    project setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+
+
+const port = 3000
+
+app.listen(port,(err)=>{
+       if(!err){
+            console.log(`app is successfully running with this ${port}`)
+       }else{
+            console.log("logging errors")
+       }
+})
