577447af77011874bb1d89ea2a556a6b6787418c
mohit nandan <mohitnandan81825@gmail.com>
2025-05-25 21:13:46 +0530

    add filters to the songs

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,4 +1,5 @@
 const { Song } = require('../models');
+const { Op } = require('sequelize');
 
 const uploadSong = async (req, res) => {
   try {
@@ -20,4 +21,25 @@ const uploadSong = async (req, res) => {
   }
 };
 
-module.exports = { uploadSong };
+const getSongs = async (req, res) => {
+  try {
+    const { title, artist, genre, releaseYear } = req.query;
+
+    const filters = {};
+
+    if (title) filters.title = { [Op.iLike]: `%${title}%` };
+    if (artist) filters.artist = { [Op.iLike]: `%${artist}%` };
+    if (genre) filters.genre = { [Op.iLike]: `%${genre}%` };
+    if (releaseYear) filters.releaseYear = releaseYear;
+
+    const songs = await Song.findAll({ where: filters });
+
+    res.status(200).json(songs);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+
+module.exports = { uploadSong , getSongs };
--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { uploadSong } = require('../controllers/songController');
+const {getSongs} = require('../controllers/songController')
 const upload = require('../middlewares/songUploader');
 
 const router = express.Router();
 
 router.post('/upload', upload.single('song'), uploadSong);
+router.get('/', getSongs);
+
 
 module.exports = router;
